815088d4874eeeca035462b44d4511125b789e67
Tomasz Reizer <t_reizer@yahoo.com>
2019-07-21 15:39:52 +0200

    added modified server.js

--- server.js
+++ server.js
@@ -1,29 +1,25 @@
 var express = require('express');
 var app = express();
-
-
 var fs = require('fs');
 var stringifyFile;
 
 app.use(bodyParser.json());
 
 app.get('/getNote', function (req, res) {
-    res.send(test.json);
-});
-
-fs.readfile('/test.json', 'utf8', function(err,data){
-  if (err) throw err;
-  stringifyFile = data;
-  res.send(data)
+  fs.readfile('/test.json', 'utf8', function(err,data){
+    if (err) throw err;
+    stringifyFile = data;
+    res.send(data)
+  });
+  res.send(test.json);
 });
 
 app.post('updateNote/:note', function(req,res){
-  res.send(req.params.note);
+  fs.writefile('/test.json', stringifyFile, function(err){
+    if (err) throw err;
+    res.send('file updated');
+  })
 });
 
-fs.writefile('/test.json', stringifyFile, function(err){
-  if (err) throw err;
-  console.log('file updated');
-})
 
 app.listen(3000);
